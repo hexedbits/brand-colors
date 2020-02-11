f6ddfddd3312b78bce36c0bef7c1acb5b5a982df
Jesse Squires <jesse@jessesquires.com>
2020-02-10 21:37:29 -0800

    restructure.

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # brand-colors
 
 *Brand colors for Hexed Bits*
-
-- red `#E0161A`
-- bright red `#F05154`
-- dark red `#54080A`
-- white `#E6DFDF`
-- black `#241C1C`
-
-[![](colors.png)](https://color.adobe.com/mythemes)
---
+++ Swift/Sources/HexedBitsColors.swift
@@ -0,0 +1,37 @@
+//
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//  Hexed Bits
+//  https://www.hexedbits.com
+//
+//  GitHub
+//  https://github.com/hexedbits/brand-colors
+//
+//  Copyright © 2019 Jesse Squires, Hexed Bits
+//
+
+import UIKit
+
+extension UIColor {
+
+	class var red: UIColor {
+		return UIColor(red: 0.88, green: 0.09, blue: 0.10, alpha: 1.0)
+	}
+
+	class var brightRed: UIColor {
+		return UIColor(red: 0.94, green: 0.32, blue: 0.33, alpha: 1.0)
+	}
+
+	class var darkRed: UIColor {
+		return UIColor(red: 0.33, green: 0.03, blue: 0.04, alpha: 1.0)
+	}
+
+	class var white: UIColor {
+		return UIColor(red: 0.90, green: 0.87, blue: 0.87, alpha: 1.0)
+	}
+
+	class var black: UIColor {
+		return UIColor(red: 0.14, green: 0.11, blue: 0.11, alpha: 1.0)
+	}
+}
